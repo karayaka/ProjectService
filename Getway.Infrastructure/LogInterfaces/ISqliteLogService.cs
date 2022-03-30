c0ce221d64fd9eee91d613a534509b1118f89afc
çağrı karayaka <kisarp_serseri@hotmail.com>
2022-03-30 10:00:46 +0300

    sqlite

--- Getway.Infrastructure/LogInterfaces/ISqliteLogService.cs
+++ Getway.Infrastructure/LogInterfaces/ISqliteLogService.cs
@@ -11,5 +11,31 @@ namespace Getway.Infrastructure.LogInterfaces
         //veritabanına özgü işlemelr yapılabilir!
         ///https://docs.microsoft.com/tr-tr/dotnet/standard/data/sqlite/?tabs=netcore-cli
         ///burdan araştırıp log create yapılacka ve win ui başlacak-
+        ///
+
+        //using (var connection = new SqliteConnection("Data Source=hello.db"))
+        //{
+        //    connection.Open();
+
+        //    var command = connection.CreateCommand();
+        //    command.CommandText =
+        //    @"
+        //        SELECT name
+        //        FROM user
+        //        WHERE id = $id
+        //    ";
+        //    command.Parameters.AddWithValue("$id", id);
+
+        //    using (var reader = command.ExecuteReader())
+        //    {
+        //        while (reader.Read())
+        //        {
+        //            var name = reader.GetString(0);
+
+        //    Console.WriteLine($"Hello, {name}!");
+        //        }
+        //    }
+        //}
+
     }
 }
